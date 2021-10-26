db42bd7d307022418716378e00e1650101f85cc3
ruchitiwari20012 <67836089+ruchitiwari20012@users.noreply.github.com>
2021-10-26 19:12:07 +0530

    Add files via upload

---
+++ comparing output.py
@@ -0,0 +1,10 @@
+#comparing the output of division(/) and floor divison(//)
+
+print(4/2)
+# division operator provides floating values result
+print(4//2)
+# floor division provides integer values result
+
+print(5/2)
+
+print(5//2)
