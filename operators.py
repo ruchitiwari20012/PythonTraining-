23acefe1beeee6d4abc06ece0c30e6a0f95bc3ab
ruchitiwari20012 <67836089+ruchitiwari20012@users.noreply.github.com>
2021-11-11 10:15:45 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,46 @@
+# Arithmetic Operators
+print(" 5+ 6 is ", 5+6)
+print(" 5- 6 is ", 5-6)
+print(" 5* 6 is ", 5*6)
+print(" 5/6 is ", 5/6)
+print(" 5//6 is ", 5//6)
+
+# Assignment Operator
+x=5
+print(x)
+x+=7
+print(x)
+x-=7
+print(x)
+x/=7
+print(x)
+
+#Comparison Operator
+i=8
+print(i==5)
+print(i!=5)# i not equal to 5
+print(i>=5)
+print(i<=5)
+
+# Logical Operator 
+a = True
+b = False
+print(a and b)
+print(a or b)
+
+# Identical Operator
+# is , is not
+
+print(a is b) # No
+print(a is not b) # yes
+
+# Membership Operators
+#in , not in 
+print("Membership Operators")
+list = [3,32,2,39,33,35]
+print(32 in list)
+print(324 not in list)
+
+print("Bitwise Operator")
+print(0 & 2)
+print( 0| 3)
