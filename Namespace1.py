e2b00bb945c06eab9043f7d973ad4ff892060f73
ruchitiwari20012 <67836089+ruchitiwari20012@users.noreply.github.com>
2021-11-12 08:22:43 +0530

    Add files via upload

---
+++ Namespace1.py
@@ -0,0 +1,11 @@
+a=10
+print(a)
+print(type(a))
+
+print(id(a))
+print(id(10))
+
+#The address for the value not for the variable a if we
+#change the value the address also got changed)
+
+#The memory handling in python is called as Namespace 
---
+++ Return value1.py
@@ -0,0 +1,7 @@
+def add():
+    a=int(input("Enter 1st No"))
+    b=int(input("Enter 2nd No"))
+    c=a+b
+    return c
+sum=add()
+print("Additional =  ",sum)
